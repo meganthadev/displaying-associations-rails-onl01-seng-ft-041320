08c330cdf48105b1aeb82cdb69a99a88372fadc2
Megan Horsfield <meganthadev@gmail.com>
2020-07-01 04:12:54 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
+<<<<<<< HEAD
 clickbait = Category.create!(name: "Motivation")
 clickbait.posts.create!(title: "10 Ways You Are Already Awesome")
 clickbait.posts.create!(title: "This Yoga Stretch Cures Procrastination, Maybe")
 clickbait.posts.create!(title: "The Power of Positive Thinking and 100 Gallons of Coffee")
  
 movies = Category.create!(name: "Movies")
+=======
+clickbait = Category.create!(name: "Motivation")
+clickbait.posts.create!(title: "10 Ways You Are Already Awesome")
+clickbait.posts.create!(title: "This Yoga Stretch Cures Procrastination, Maybe")
+clickbait.posts.create!(title: "The Power of Positive Thinking and 100 Gallons of Coffee")
+ 
+movies = Category.create!(name: "Movies")
+>>>>>>> f2cb8a119964e4b5e9e1df1ef4cfccb1e61e141f
 movies.posts.create!(title: "Top 20 Summer Blockbusters Featuring a Cute Dog")
